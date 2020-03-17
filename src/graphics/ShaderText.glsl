480861e30e62452ab821392dff7af13884a1016e
Conor Long <conorlong99@gmail.com>
2020-03-17 13:05:28 -0400

    Update ShaderText.glsl

--- src/graphics/ShaderText.glsl
+++ src/graphics/ShaderText.glsl
@@ -1,6 +1,5 @@
 #version 450
 out vec4 FragColor;
-out vec4 moreColors;
 
 in vec3 ourColor;
 in vec2 TexCoord;
@@ -10,7 +9,6 @@ uniform sampler2D texture1;
 void main()
 {
     FragColor = texture(texture1, TexCoord);
-	moreColors = texture(texture1, TexCoord);
 } 
 
 
