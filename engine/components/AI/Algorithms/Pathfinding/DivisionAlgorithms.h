59499130762ca1d5625fb7f28ecd23bfa62284f7
Tristan Fish <tfishyy@gmail.com>
2021-11-09 08:31:23 -0500

    Forgot to include link

--- engine/components/AI/Algorithms/Pathfinding/DivisionAlgorithms.h
+++ engine/components/AI/Algorithms/Pathfinding/DivisionAlgorithms.h
@@ -4,7 +4,7 @@
 namespace DivisionAlgorithms
 {
     //http://www.cs.kent.edu/~dragan/ST-Spring2016/visibility%20graphs.pdf
-
+	//http://www.science.smith.edu/~istreinu/Teaching/Courses/274/Spring98/Projects/Philip/fp/algVisibility.htm
 	class BaseDivision
 	{
 	private:
