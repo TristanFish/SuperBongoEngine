739ff60a54138f5e1c15b6e7e32c48b14aef32e9
Tristan Fish <tfishyy@gmail.com>
2021-11-09 08:23:54 -0500

    Started On Divisional Algorithms

---
+++ engine/components/AI/Algorithms/Pathfinding/DivisionAlgorithms.h
@@ -0,0 +1,21 @@
+#ifndef DIVISIONALGORITHMS_H
+#define DIVISIONALGORITHMS_H
+
+namespace DivisionAlgorithms
+{
+    //http://www.cs.kent.edu/~dragan/ST-Spring2016/visibility%20graphs.pdf
+
+	class BaseDivision
+	{
+	private:
+
+	public:
+
+	};
+
+
+
+
+}
+#endif
+
